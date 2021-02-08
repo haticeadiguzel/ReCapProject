4ab43ce1e6d18ac51bac1a82d399062878c6a6c8
hatice adıgüzel <adiguzell.hatice@gmail.com>
2021-02-08 20:20:38 +0300

    Add project files.

---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,58 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Car> _cars;
+        public InMemoryProductDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{Id=1, BrandId=1, ColorId=1, ModelYear=2015, DailyPrice = 150, Description="Hatasız." },
+                new Car{Id=2, BrandId=1, ColorId=1, ModelYear=1995, DailyPrice = 100, Description="Hatasız." },
+                new Car{Id=3, BrandId=2, ColorId=1, ModelYear=2019, DailyPrice = 250, Description="Hatasız." },
+            };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car CarToDelete = _cars.First(c=>c.Id == car.Id);
+            _cars.Remove(CarToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        //public List<Car> GetByld(int id)
+        //{return _cars.Where(c => c.Id == id).ToList();}
+
+        public List<Car> GetByld()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.First(c => c.Id == car.Id);
+            carToUpdate.Id = car.Id;
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+        }
+    }
+}
